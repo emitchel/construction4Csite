aae9aeee1d4c4c8fff45e693dde7eb3604f97bb6
Elliot Mitchell <elliot.r.mitchell@gmail.com>
2017-06-20 07:18:35 -0500

    Beginnings of CSS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,50 @@
 //= require jquery
 //= require rails-ujs
 //= require turbolinks
-//= require_tree .
+//= require jquery-migrate.min.js
+//= require bootstrap.min.js
+//= require bootstrap-hover-dropdown.min.js
+//= require wow.min.js
+//= require dzsparallaxer.js
+//= require bootstrap-hover-dropdown.min.js
+//= require owl.carousel.min.js
+//= require custom.js
+//= require masterslider.min.js
+//= require master-custom.js
+//= require jquery.cubeportfolio.min.js
+
+(function ($, window, document, undefined) {
+    'use strict';
+
+    // init cubeportfolio
+    $('#js-grid-agency').cubeportfolio({
+        filters: '#js-filters-agency',
+        loadMore: '#js-loadMore-agency',
+        loadMoreAction: 'click',
+        layoutMode: 'grid',
+        defaultFilter: '*',
+        animationType: 'slideLeft',
+        gapHorizontal: 15,
+        gapVertical: 0,
+        gridAdjustment: 'responsive',
+        mediaQueries: [{
+                width: 1500,
+                cols: 4
+            }, {
+                width: 1100,
+                cols: 4
+            }, {
+                width: 800,
+                cols: 3
+            }, {
+                width: 480,
+                cols: 2
+            }, {
+                width: 320,
+                cols: 1
+            }],
+        caption: 'zoom',
+        displayType: 'fadeIn',
+        displayTypeSpeed: 100
+    });
+    })(jQuery, window, document);
