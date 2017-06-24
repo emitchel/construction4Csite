1f4be400f79628dd68f25ec19e2ee148575dbbff
Elliot Mitchell <elliot.r.mitchell@gmail.com>
2017-06-23 21:11:11 -0500

    color and some links in place

---
+++ lib/constants.rb
@@ -0,0 +1,9 @@
+module Constants
+    URL_FACEBOOK = 'https://www.facebook.com/4cconstructionllc/'
+
+    PHONE_NUMBER = '(402) 616-1814'
+
+    EMAIL = 'bojan.ninkovic@yahoo.com'
+
+    TITLE = '4C Construction'
+end
