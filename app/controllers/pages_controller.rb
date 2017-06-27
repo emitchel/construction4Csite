349880c7aa8b40e4f5823750700fe67c308fba37
Elliot Mitchell <elliot.r.mitchell@gmail.com>
2017-06-26 19:50:23 -0500

    base functionality for services down

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,61 @@
 class PagesController < ApplicationController
+  before_action :check_service, only: [:services]
+
   def index
+  end
+
+  def services
+    service = service_content
+    @title = service[:title]
+    @subtitle = service[:subtitle]
+    @description = service[:description]
+    @background_picture = service[:background_picture]
+    @picture1 = service[:picture1]
+    @picture2 = service[:picture2]
+    @picture3 = service[:picture3]
+  end
+
+  private
+
+  SUPPORTED_SERVICES = %w[renovation carpentry masonry].freeze
+
+  RENOVATION_SERVICE = { title: 'Renovation Service',
+                         subtitle: 'House Renovation',
+                         description: 'We do house renovations',
+                         background_picture: '/assets/bath1.jpg',
+                         picture1: '/assets/reno1.jpg',
+                         picture2: '/assets/reno2.jpg',
+                         picture3: '/assets/familytokitchen.jpg' }.freeze
+  CARPENTRY_SERVICE = { title: 'Renovation Service',
+                        subtitle: 'House Renovation',
+                        description: 'We do house renovations',
+                        background_picture: '/assets/bath1.jpg',
+                        picture1: '/assets/reno1.jpg',
+                        picture2: '/assets/reno2.jpg',
+                        picture3: '/assets/familytokitchen.jpg' }.freeze
+  MASONRY_SERVICE = { title: 'Renovation Service',
+                      subtitle: 'House Renovation',
+                      description: 'We do house renovations',
+                      background_picture: '/assets/bath1.jpg',
+                      picture1: '/assets/reno1.jpg',
+                      picture2: '/assets/reno2.jpg',
+                      picture3: '/assets/familytokitchen.jpg' }.freeze
+
+  def check_service
+    raise ActionController::RoutingError.new('Not Found') unless SUPPORTED_SERVICES.include?(service)
+  end
+
+  def service
+    params[:service]
+  end
+
+  def service_content
+    if service == 'renovation'
+      RENOVATION_SERVICE
+    elsif service == 'carpentry'
+      CARPENTRY_SERVICE
+    elsif service == 'masonry'
+      MASONRY_SERVICE
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/index' => 'pages#index'
+  get 'services/:service' => 'pages#services'
+
   root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
