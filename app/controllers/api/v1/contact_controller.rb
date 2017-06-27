be01793c8680b52a8529f9caff3469675c2fda5a
Elliot Mitchell <elliot.r.mitchell@gmail.com>
2017-06-27 10:21:33 -0500

    beginnings of contact controller

---
+++ app/controllers/api/v1/contact_controller.rb
@@ -0,0 +1,5 @@
+class ContactController < Api::V1::BaseController
+  def create
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,4 @@ Rails.application.routes.draw do
   get 'contact' => 'pages#contact'
 
   root 'pages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
