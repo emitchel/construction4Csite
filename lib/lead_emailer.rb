6672f9994719be031d6aad06ba4aaa57379b95e0
Elliot Mitchell <elliot.r.mitchell@gmail.com>
2017-06-27 16:02:37 -0500

    new images, still working on email format

--- lib/lead_emailer.rb
+++ lib/lead_emailer.rb
@@ -10,7 +10,7 @@ module LeadEmailer
       from = Email.new(email: LEAD_EMAIL_FROM)
       to = Email.new(email: LEAD_EMAIL_TO)
       subject = 'New 4C Lead'
-      content = Content.new(type: 'text/plain', value: LeadEmailer.value_from_lead(lead))
+      content = Content.new(type: 'html', value: LeadEmailer.value_from_lead(lead))
       mail = Mail.new(from, subject, to, content)
       sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
       response = sg.client.mail._('send').post(request_body: mail.to_json)
@@ -25,12 +25,12 @@ module LeadEmailer
   end
 
   def self.value_from_lead(lead)
-    value = 'New Lead for 4C Construction \n'
-    value = value + 'Name: #{lead.name} \n'
-    value = value + 'Email: #{lead.email} \n'
-    value = value + 'How they found out about us: #{lead.source} \n'
-    value = value + 'Message: #{lead.message} \n'
-    value + 'Lead created at: #{lead.created_at} \n'
+    value = 'New Lead for 4C Construction <br>'
+    value += "Name: #{lead.name} <br>"
+    value += "Email: #{lead.email} <br>"
+    value += "How they found out about us: #{lead.source} <br>"
+    value += "Message: #{lead.message} <br>"
+    value + "Lead created at: #{lead.created_at} <br>"
   end
 
 end
