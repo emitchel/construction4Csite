f7990e8cb4d680372dc17f56af17d20c0b937323
Elliot Mitchell <elliot.r.mitchell@gmail.com>
2017-07-07 09:56:54 -0500

    putting in text and tweaking pictures

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,16 +2,19 @@ class PagesController < ApplicationController
   before_action :check_service, only: [:services]
   skip_before_action :verify_authenticity_token, only: [:contact_us]
 
-  def index; end
+  def index;
+  end
 
-  def about; end
+  def about;
+  end
 
-  def contact; end
+  def contact;
+  end
 
   def contact_us
     lead = Lead.new(name: params[:name], email: params[:email], message: params[:message], source: params[:how])
     lead.save
-    render json: { success: LeadEmailer.email_lead(lead) }
+    render json: {success: LeadEmailer.email_lead(lead)}
   end
 
 
@@ -34,26 +37,26 @@ class PagesController < ApplicationController
   SUPPORTED_SERVICES = %w[renovation carpentry homes].freeze
 
   RENOVATION_SERVICE = { title: 'Renovation Service',
-                         subtitle: 'House Renovation',
-                         description: 'We do house renovations',
-                         background_picture: '/assets/bath1.jpg',
-                         picture1: '/assets/reno1.jpg',
-                         picture2: '/assets/reno2.jpg',
-                         picture3: '/assets/familytokitchen.jpg' }.freeze
+                        subtitle: 'House Renovation',
+                        description: 'Bathrooms, kitchens, bedrooms, entire basements, renovations are a great route for adding that personal touch',
+                        background_picture: '/assets/bath1.jpg',
+                        picture1: '/assets/reno1.jpg',
+                        picture2: '/assets/reno2.jpg',
+                        picture3: '/assets/familytokitchen.jpg' }.freeze
   CARPENTRY_SERVICE = { title: 'Carpentry Service',
-                        subtitle: 'Decks & Carpentry',
-                        description: 'We do decks and variations of carpentry',
-                        background_picture: '/assets/15.jpg',
-                        picture1: '/assets/5.jpg',
-                        picture2: '/assets/goodeck.jpg',
-                        picture3: '/assets/familyroom3.jpg' }.freeze
+                       subtitle: 'Decks & Carpentry',
+                       description: 'While we do specialize in customized decks, any type of carpentry isn\'t out of the question',
+                       background_picture: '/assets/15.jpg',
+                       picture1: '/assets/5.jpg',
+                       picture2: '/assets/goodeck.jpg',
+                       picture3: '/assets/familyroom3.jpg' }.freeze
   HOME_BUILDING_SERVICE = { title: 'Home Building Service',
-                            subtitle: 'Custom Home Building',
-                            description: 'We do various concrete services',
-                            background_picture: '/assets/blueprint1.jpg',
-                            picture1: '/assets/Front.jpg',
-                            picture2: '/assets/3.jpg',
-                            picture3: '/assets/basement1.jpg' }.freeze
+                           subtitle: 'Custom Home Building',
+                           description: 'As far as custom home building we do everything. Custom kitchens, bathrooms, living area, bedrooms. From floor to ceiling, whatever you need done, we can do it.',
+                           background_picture: '/assets/blueprint1.jpg',
+                           picture1: '/assets/Front.jpg',
+                           picture2: '/assets/3.jpg',
+                           picture3: '/assets/basement1.jpg' }.freeze
 
   def check_service
     raise ActionController::RoutingError.new('Not Found') unless SUPPORTED_SERVICES.include?(service)
