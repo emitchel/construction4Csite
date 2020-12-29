ace337d38517327b46660679e1dc69916d716c4e
Elliot Mitchell <elliot.r.mitchell@gmail.com>
2020-12-28 23:42:11 -0600

    Initial efforts + cloudinary setup

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+    config.cloud_name = 'hddupyjhs'
+    config.api_key = '431998789442742'
+    config.api_secret = '91ek4azKknZeKRfVswyYAygAKw4'
+    config.secure = true
+    config.cdn_subdomain = true
+  end
